de5c58df3118dd2d95d78ffe5913e7da8f147af8
MarceloHcb <marcelohcb@msn.com>
2023-06-08 14:02:26 -0300

    Cria arquivo index req13

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('player1');
+const player2 = new Character('player2');
+const player3 = new Character('player3');
+
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battle:Battle[]):void {
+  battle.forEach((bt) => {
+    bt.fight();
+  });
+}
+
+export { runBattles, pvp, pve, player1, player2, player3, monster1, monster2 };
